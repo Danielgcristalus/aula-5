a2a5984185f1c6d522776a34a426dcffc5bd988b
Danielgcristalus <danielgcristalus@gmail.com>
2025-02-12 15:39:19 -0300

    Add files via upload

---
+++ exercise day5.py
@@ -0,0 +1,45 @@
+#1
+print ('Boas Vindas')
+#2
+verdadeiro = True
+print(type(verdadeiro))
+#3
+numero1 = 3
+numero2 = 2
+print(numero1 * numero2)
+#4
+inteiro1 = 10
+inteiro2 = 2
+print(inteiro1 / inteiro2)
+#5
+inteiro3 = 2
+inteiro4 = 1
+print(inteiro3 - inteiro4)
+#6
+inteiro5 = 20
+inteiro6 = 4
+print(inteiro5 // inteiro6)
+#7
+dec1 = 1
+dec2 = 2
+dec3 = 3
+dec4 = 4
+print(dec1 * dec2 * dec3 * dec4)
+#8
+numero = 5
+print(numero * 2)
+#9
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+soma = num1 + num2
+print("A soma é:", soma)
+#10
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+email = input("Digite seu email: ")
+
+print("\nCadastro realizado com sucesso!")
+print("Nome:", nome)
+print("Idade:", idade)
+print("Email:", email)
+
